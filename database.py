e8a9ab3e2c8090ebd7900ebe2d32858597f464d9
masserep <pierre.masserey@unifr.ch>
2024-05-30 20:35:23 +0200

    second commit*

--- database.py
+++ database.py
@@ -1,30 +1,82 @@
 import pandas as pd
 import networkx as nx
-import skfuzzy as fuzz
-import numpy as np
 
 # Données sur les trajets
-data = {
-    'From': ['Minfeng', 'Minfeng', 'Yutian', 'Yutian', 'Minfeng', 'Minfeng', 'Luntai', 'Luntai', 'Kuqa', 'Kuqa', 'Aksu', 'Aksu', 'Atushi / Atux', 'Atushi / Atux', 'Kashgar / Kashi', 'Kashgar / Kashi', 'Kashgar / Kashi', 'Taxkorgan', 'Yarkand', 'Yarkand', 'Yarkand', 'Yarkand', 'Hotan', 'Hotan', 'Qiao mo/Qiemo', 'Qiao mo/Qiemo', 'Ruoqiang', 'Ruoqiang', 'Kuqa', 'Korler/Korla', 'Korler/Korla', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Korler/Korla', 'Korler/Korla', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Karamay', 'Karamay', 'Korler/Korla', 'Korler/Korla', 'Korler/Korla', 'Turpan', 'Hotan', 'Alaer', 'Alaer', 'Kuqa', 'Kuqa', 'Yili/Yining', 'Yili/Yining'],
+routes_data = {
+    'From': ['Minfeng', 'Minfeng', 'Yutian', 'Yutian', 'Minfeng', 'Minfeng', 'Luntai', 'Luntai', 'Kuqa', 'Kuqa', 'Aksu', 'Aksu', 'Atushi / Atux', 'Atushi / Atux', 'Kashgar', 'Kashgar', 'Kashgar', 'Taxkorgan', 'Yarkand', 'Yarkand', 'Yarkand', 'Yarkand', 'Hotan', 'Hotan', 'Qiao mo/Qiemo', 'Qiao mo/Qiemo', 'Ruoqiang', 'Ruoqiang', 'Kuqa', 'Korler/Korla', 'Korler/Korla', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Xinyuan', 'Korler/Korla', 'Korler/Korla', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Karamay', 'Karamay', 'Korler/Korla', 'Korler/Korla', 'Korler/Korla', 'Turpan', 'Hotan', 'Alaer', 'Alaer', 'Kuqa', 'Kuqa', 'Yili/Yining', 'Yili/Yining'],
     'To': ['Qiao mo/Qiemo', 'Qiao mo/Qiemo', 'Minfeng', 'Minfeng', 'Luntai', 'Luntai', 'Korler/Korla', 'Korler/Korla', 'Luntai', 'Luntai', 'Kuqa', 'Kuqa', 
-'Aksu', 'Aksu', 'Atushi / Atux', 'Atushi / Atux', 'Atushi / Atux', 'Kashi', 'Hotan', 'Kashgar', 'Kashgar', 'Kashgar', 'Yutian', 'Yutian', 'Ruoqiang', 'Ruoqiang', 'Korler/Korla', 'Korler/Korla', 'Xinyuan', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Urumqi', 'Urumqi', 'Urumqi', 'Urumqi', 'Urumqi', 'Urumqi', 'Karamay', 'Urumqi', 'Urumqi', 'Turpan', 'Turpan', 'Turpan', 'Urumqi', 'Alaer/Aral', 'Kuqa', 'Kuqa', 'Yili/Yining', 'Yili/Yining', 'Xinyuan', 'Xinyuan'],
-    'km': ['300', '311', '110', '114', '575', '733', '176', '222', '115', '123', '290', '250', '426', '487', '45', '45', '45', '290', '315', '190', '237', 
-'192', '201', '217', '280', '280', '415', '430', '505', '700', '760', '290', '634', '419', '518', '871', '695', '482', '524', '240', '154', '316', '358', '415', '377', '427', '210', '438', '230', '313', '590', '900', '184', '184'],
-    'time': ['3h00', '4h', '1h45', '1h25', '8h10', '9h', '2h30', '3h30', '1h15', '2h15', '4h', '3h', '4h30', '5h40', '0h50', '0h50', '1h30', '4h45', '3h45', 
-'2h30', '2h45', '3h45', '3h40', '2h50', '3h00', '3h30', '4h', '5h', '6h00', '8h', '10h30', '3h45', '7h15', '7h20', '6h00', '9h30', '11h20', '6h', '8h', '2h15', '1h45', '3h30', '4h40', '5h10', '5h10', '7h10', '2h45', '5h30', '4h', '3h40', '6h', '13h20', '2h', '3h'],
-    'Type of road': ['Highway', 'Highway+freeway', 'Freeway', 'Highway', 'Desert road', 'Highway+freeway', 'Highway', 'Highway+Desert road+freeway', 'Highway', 'Freeway', 'Freeway', 'Highway', 'Highway', 'Highway+freeway', 'Highway', 'Highway+freeway', 'Freeway', 'Freeway', 'Highway', 'Highway+freeway', 'Highway', 'Freeway', 'Freeway', 'Highway', 'Highway', 'Highway+freeway', 'Highway', 'Highway+freeway', '', 'Highway', 'Highway+freeway', 'Road closure', 'Highway', 'Freeway', 'Road closure', 'Highway', 'Highway+freeway', 'Highway', 'Highway+freeway', 'Highway', 'Highway', 'Highway', 'Highway+freeway', 'Highway', 'Highway+freeway', 'Freeway', 'Highway', 'Freeway', 'Freeway', 'Highway+freeway', 'Road closure', 'Highway+freeway', 'Highway', 'Highway+freeway']
+'Aksu', 'Aksu', 'Atushi / Atux', 'Atushi / Atux', 'Atushi / Atux', 'Kashgar', 'Hotan', 'Kashgar', 'Kashgar', 'Kashgar', 'Yutian', 'Yutian', 'Ruoqiang', 'Ruoqiang', 'Korler/Korla', 'Korler/Korla', 'Xinyuan', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Kuitun / Kuytun', 'Urumqi', 'Urumqi', 'Urumqi', 'Urumqi', 'Urumqi', 'Urumqi', 'Karamay', 'Urumqi', 'Urumqi', 'Turpan', 'Turpan', 'Turpan', 'Urumqi', 'Alaer', 'Kuqa', 'Kuqa', 'Yili/Yining', 'Yili/Yining', 'Xinyuan', 'Xinyuan'],
+    'km': [300, 311, 110, 114, 575, 733, 176, 222, 115, 123, 290, 250, 426, 487, 45, 45, 45, 290, 315, 190, 237, 
+192, 201, 217, 280, 280, 415, 430, 505, 700, 760, 290, 634, 419, 518, 871, 695, 482, 524, 240, 154, 316, 358, 415, 377, 427, 210, 438, 230, 313, 590, 900, 184, 184],
+    'time': [180,
+240,
+105,
+85,
+490,
+540,
+150,
+210,
+75,
+135,
+240,
+180,
+270,
+340,
+50,
+50,
+90,
+285,
+225,
+150,
+165,
+225,
+220,
+170,
+180,
+210,
+240,
+300,
+360,
+480,
+630,
+225,
+435,
+440,
+360,
+570,
+680,
+360,
+480,
+135,
+105,
+210,
+280,
+310,
+310,
+430,
+165,
+330,
+240,
+220,
+360,
+800,
+120,
+180,
+]
+
+,
 }
 
 # Vérifiez les longueurs des listes pour les trajets
 print("Vérification des longueurs des listes pour les trajets:")
-for key, value in data.items():
+for key, value in routes_data.items():
     print(f'Length of {key}: {len(value)}')
 
 # Données sur les villes
 cities_data = {
-    'City': ['Minfeng', 'Qiemo', 'Yutian', 'Luntai', 'Korler', 'Kuqa', 'Aksu', 'Atushi', 'Kashi', 
-             'Taxkorgan', 'Yarkand', 'Hotan', 'Ruoqiang', 'Xinyuan', 'Kuitun', 'Urumqi', 'Karamay', 
-             'Turpan', 'Alaer', 'Yili'],
+    'City': ['Minfeng', 'Qiao mo/Qiemo', 'Yutian', 'Luntai', 'Korler/Korla', 'Kuqa', 'Aksu', 'Atushi / Atux', 'Kashgar', 
+             'Taxkorgan', 'Yarkand', 'Hotan', 'Ruoqiang', 'Xinyuan', 'Kuitun / Kuytun', 'Urumqi', 'Karamay', 
+             'Turpan', 'Alaer', 'Yili/Yining'],
     'Natural scenery': [80, 20, 50, 80, 50, 50, 70, 20, 20, 100, 20, 10, 45, 100, 0, 25, 85, 28, 40, 20],
     'City sight': [0, 20, 0, 0, 0, 0, 0, 0, 20, 0, 0, 10, 0, 0, 0, 25, 0, 0, 0, 20],
     'Historical spot': [20, 20, 20, 20, 25, 30, 20, 0, 30, 0, 40, 50, 40, 0, 0, 25, 15, 44, 0, 20],
@@ -39,7 +91,7 @@ for key, value in cities_data.items():
 
 # Créez les DataFrames après avoir corrigé les longueurs
 try:
-    routes_df = pd.DataFrame(data)
+    routes_df = pd.DataFrame(routes_data)
     cities_df = pd.DataFrame(cities_data)
     print("DataFrames créés avec succès.")
 except ValueError as e:
@@ -51,9 +103,10 @@ print(cities_df)
 # Créez le graphe pondéré
 G = nx.Graph()
 
+
 # Ajoutez les trajets au graphe
 for index, row in routes_df.iterrows():
-    G.add_edge(row['From'], row['To'], weight=row['km'], time=row['time'], road_type=row['Type of road'])
+    G.add_edge(row['From'], row['To'], weight=row['km'], time=row['time'])
 
 # Ajoutez les attributs des villes au graphe
 for index, row in cities_df.iterrows():
--- main.py
+++ main.py
@@ -1,96 +1,180 @@
-import skfuzzy as fuzz
-import skfuzzy.control as ctrl
 import networkx as nx
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from database import routes_df, cities_df, G
-
-# Définir les ensembles flous pour chaque type de préférence
-natural_scenery = ctrl.Antecedent(np.arange(0, 101, 1), 'natural_scenery')
-city_sight = ctrl.Antecedent(np.arange(0, 101, 1), 'city_sight')
-historical_spot = ctrl.Antecedent(np.arange(0, 101, 1), 'historical_spot')
-cultural_experience = ctrl.Antecedent(np.arange(0, 101, 1), 'cultural_experience')
-rest = ctrl.Antecedent(np.arange(0, 101, 1), 'rest')
-
-# Définir les fonctions d'appartenance floues
-natural_scenery.automf(3)
-city_sight.automf(3)
-historical_spot.automf(3)
-cultural_experience.automf(3)
-rest.automf(3)
-
-# Définir la variable de sortie
-preference = ctrl.Consequent(np.arange(0, 101, 1), 'preference')
-preference.automf(3)
-
-# Règles floues pour déterminer les préférences
-rule1 = ctrl.Rule(natural_scenery['good'] & city_sight['poor'] & historical_spot['average'] & cultural_experience['poor'] & rest['average'], preference['good'])
-rule2 = ctrl.Rule(natural_scenery['average'] & city_sight['average'] & historical_spot['good'] & cultural_experience['good'] & rest['poor'], preference['average'])
-rule3 = ctrl.Rule(natural_scenery['poor'] & city_sight['good'] & historical_spot['poor'] & cultural_experience['average'] & rest['good'], preference['poor'])
-
-# Système de contrôle flou
-preference_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
-preference_sim = ctrl.ControlSystemSimulation(preference_ctrl)
-
-
-# Ajouter les arêtes au graphe
-for _, row in routes_df.iterrows():
-    G.add_edge(row['From'], row['To'], weight=row['km'])
+from tkinter import Tk, Label, Button, StringVar, Listbox, END, MULTIPLE, OptionMenu, Scale, HORIZONTAL
+from database import G, routes_df, cities_data
 
 
 
-# Ajouter les attributs des villes au graphe
-for _, row in cities_df.iterrows():
-    G.nodes[row['City']].update({
-        'natural_scenery': row['Natural scenery'],
-        'city_sight': row['City sight'],
-        'historical_spot': row['Historical spot'],
-        'cultural_experience': row['Cultural experience'],
-        'rest': row['Rest']
-    })
+# Définir le graphe
+G = nx.Graph()
 
-# Calculer les scores flous pour chaque ville
-city_scores = {}
+# Ajoutez les trajets au graphe avec l'attribut 'distance'
+for index, row in routes_df.iterrows():
+    G.add_edge(row['From'], row['To'], distance=row['km'], time=row['time'])
 
-for city in G.nodes():
-    attrs = G.nodes[city]
-    preference_sim.input['natural_scenery'] = attrs['natural_scenery']
-    preference_sim.input['city_sight'] = attrs['city_sight']
-    preference_sim.input['historical_spot'] = attrs['historical_spot']
-    preference_sim.input['cultural_experience'] = attrs['cultural_experience']
-    preference_sim.input['rest'] = attrs['rest']
-    preference_sim.compute()
-    city_scores[city] = preference_sim.output['preference']
 
-# Trier les villes en fonction des scores"""
-sorted_cities = sorted(city_scores.items(), key=lambda x: x[1], reverse=True)
-print("Villes classées par score de préférence:", sorted_cities)
+def dijkstra_shortest_path(graph, start, end):
+    shortest_paths = nx.shortest_path(graph, source=start, weight='distance')
+    shortest_path = shortest_paths[end]
 
-# Sélectionner les meilleures villes (par exemple, les 5 premières)
-top_cities = [city for city, score in sorted_cities[:5]]
-print("Meilleures villes à visiter:", top_cities)
+    total_distance = sum(graph[shortest_path[i]][shortest_path[i+1]]['distance'] for i in range(len(shortest_path) - 1))
 
-# Trouver les meilleurs trajets entre les villes sélectionnées
-def find_best_path(graph, cities):
-    paths = []
-    for i in range(len(cities) - 1):
-        path = nx.shortest_path(graph, source=cities[i], target=cities[i + 1], weight='weight')
-        paths.append(path)
-    return paths
+    return shortest_path, total_distance
 
-best_paths = find_best_path(G, top_cities)
-print("Meilleurs trajets entre les villes sélectionnées:", best_paths)
+def create_custom_route(graph, start, end, waypoints):
+    total_path = []
+    total_distance = 0
 
-# Visualiser le graphe et les trajets
+    # Initial starting point
+    current_start = start
 
-# Dessiner le graphe
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True, node_size=3000, node_color='skyblue', font_size=10, font_weight='bold', edge_color='gray')
+    for waypoint in waypoints:
+        path_segment, distance_segment = dijkstra_shortest_path(graph, current_start, waypoint)
+        total_path += path_segment[:-1]  # Add path segment, except the last node to avoid duplication
+        total_distance += distance_segment
+        current_start = waypoint
 
-# Mettre en évidence les meilleurs trajets
-for path in best_paths:
-    path_edges = list(zip(path, path[1:]))
-    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='red', width=2)
+    # Final segment to the destination
+    path_segment, distance_segment = dijkstra_shortest_path(graph, current_start, end)
+    total_path += path_segment
+    total_distance += distance_segment
 
-plt.show()
+    return total_path, total_distance
+
+# Cette fonction draw_graph est également identique à celle que vous avez fournie.
+def draw_graph(G, shortest_path, custom_path):
+    plt.figure(figsize=(14, 8))
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G, pos, with_labels=True, node_size=1000, node_color='skyblue', font_size=10, font_weight='bold')
+
+    labels = {(u, v): f"{data['distance']}km\n{data['time']}min" for u, v, data in G.edges(data=True)}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
+    # Highlight the custom path in green first
+    green_edges = list(zip(custom_path, custom_path[1:]))
+    edge_colors = ['green' if edge in green_edges or (edge[1], edge[0]) in green_edges else 'black' for edge in G.edges()]
+    nx.draw_networkx_edges(G, pos, edgelist=green_edges, edge_color='green', width=2)
+
+    # Highlight the shortest path in red after the green path
+    red_edges = list(zip(shortest_path, shortest_path[1:]))
+    edge_colors = ['red' if edge in red_edges or (edge[1], edge[0]) in red_edges else edge_colors[i] for i, edge in enumerate(G.edges())]
+    nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='red', width=2)
+
+    plt.show()
+
+
+
+def get_city_scores(city_name):
+    city_index = cities_data['City'].index(city_name)
+    natural_scenery_score = cities_data['Natural scenery'][city_index]
+    city_sight_score = cities_data['City sight'][city_index]
+    historical_spot_score = cities_data['Historical spot'][city_index]
+    cultural_experience_score = cities_data['Cultural experience'][city_index]
+    rest_score = cities_data['Rest'][city_index]
+    return {
+        'natural_scenery': natural_scenery_score,
+        'city_sight': city_sight_score,
+        'historical_spot': historical_spot_score,
+        'cultural_experience': cultural_experience_score,
+        'rest': rest_score
+    }
+
+
+# Cette fonction on_calculate est également identique à celle que vous avez fournie.
+def on_calculate():
+    start_node = start_var.get()
+    end_node = end_var.get()
+    waypoints = [waypoint_listbox.get(i) for i in waypoint_listbox.curselection()]
+
+    # Get preferences from scales
+    natural_scenery_pref = natural_scenery_scale.get()
+    city_sight_pref = city_sight_scale.get()
+    historical_spot_pref = historical_spot_scale.get()
+    cultural_experience_pref = cultural_experience_scale.get()
+    rest_pref = rest_scale.get()
+
+    # Calculate score for each city based on preferences
+    city_scores = {}
+    for city in sorted(G.nodes()):
+        scores = get_city_scores(city)
+        score = (natural_scenery_pref * scores['natural_scenery'] +
+                 city_sight_pref * scores['city_sight'] +
+                 historical_spot_pref * scores['historical_spot'] +
+                 cultural_experience_pref * scores['cultural_experience'] +
+                 rest_pref * scores['rest'])
+        city_scores[city] = score
+
+    # Sélectionner les villes les mieux adaptées
+    suitable_cities = sorted(city_scores.keys(), key=lambda x: city_scores[x], reverse=True)[:3]  # Sélectionnez les 5 meilleures villes
+
+    # Calculer l'itinéraire avec les villes sélectionnées
+    shortest_path, total_distance = dijkstra_shortest_path(G, start_node, end_node)
+    custom_path, custom_distance = create_custom_route(G, start_node, end_node, waypoints + suitable_cities)
+
+    # Afficher les résultats
+    shortest_path_text.set(f"Shortest Path (Red): {' -> '.join(shortest_path)}\nTotal distance: {total_distance} km")
+    custom_path_text.set(f"Custom Path (Green): {' -> '.join(custom_path)}\nTotal distance: {custom_distance} km")
+
+    draw_graph(G, shortest_path, custom_path)
+
+
+# Vous pouvez ajouter vos données de graphe ici.
+# Par exemple:
+# G.add_edges_from(edges)
+
+# Configuration de l'interface utilisateur Tkinter
+# Configuration de l'interface utilisateur Tkinter
+# Configuration de l'interface utilisateur Tkinter
+root = Tk()
+root.title("Personnalised.Itinary")
+
+window_width = 1200
+window_height = 800
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_cordinate = int((screen_width/2) - (window_width/2))
+y_cordinate = int((screen_height/2) - (window_height/2))
+root.geometry(f"{window_width}x{window_height}+{x_cordinate}+{y_cordinate}")
+
+Label(root, text="Starting Point:").grid(row=0, column=0, padx=10, pady=10)
+start_var = StringVar()
+start_menu = OptionMenu(root, start_var, *sorted(G.nodes()))
+start_menu.grid(row=0, column=1, padx=10, pady=10)
+
+Label(root, text="Arriving Point:").grid(row=1, column=0, padx=10, pady=10)
+end_var = StringVar()
+end_menu = OptionMenu(root, end_var, *sorted(G.nodes()))
+end_menu.grid(row=1, column=1, padx=10, pady=10)
+
+Label(root, text="Intermediary Points:").grid(row=2, column=0, padx=10, pady=10)
+waypoint_listbox = Listbox(root, selectmode=MULTIPLE, height=10, width=30)
+waypoint_listbox.grid(row=2, column=1, padx=10, pady=10)
+for node in sorted(G.nodes()):
+    waypoint_listbox.insert(END, node)
+
+Label(root, text="Preferences:").grid(row=3, column=0, padx=10, pady=10)
+natural_scenery_scale = Scale(root, from_=1, to=5, orient=HORIZONTAL, label="Natural Scenery")
+natural_scenery_scale.grid(row=3, column=1, padx=10, pady=5)
+
+city_sight_scale = Scale(root, from_=1, to=5, orient=HORIZONTAL, label="City Sight")
+city_sight_scale.grid(row=4, column=1, padx=10, pady=5)
+
+historical_spot_scale = Scale(root, from_=1, to=5, orient=HORIZONTAL, label="Historical Spot")
+historical_spot_scale.grid(row=5, column=1, padx=10, pady=5)
+
+cultural_experience_scale = Scale(root, from_=1, to=5, orient=HORIZONTAL, label="Cultural Experience")
+cultural_experience_scale.grid(row=6, column=1, padx=10, pady=5)
+
+rest_scale = Scale(root, from_=1, to=5, orient=HORIZONTAL, label="Rest")
+rest_scale.grid(row=7, column=1, padx=10, pady=5)
+
+
+Button(root, text="Calculate Itinerary", command=on_calculate).grid(row=8, column=1, padx=10, pady=10)
+
+shortest_path_text = StringVar()
+custom_path_text = StringVar()
+
+Label(root, textvariable=shortest_path_text, fg='red', wraplength=1000).grid(row=9, column=0, columnspan=2, sticky='w', padx=10, pady=10)
+Label(root, textvariable=custom_path_text, fg='green', wraplength=1000).grid(row=10, column=0, columnspan=2, sticky='w', padx=10, pady=10)
+
+root.mainloop()
